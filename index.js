21afad81bc61b054812bb24eaf828857806f3f3a
Omar Rizwan <innovativewebpage@gmail.com>
2025-03-07 15:01:18 +0530

    hi

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
   <body>
     <h3>Ilife</h3>
     <h3>Hi Omar this is new changes</h3>
-    <h3>Hi Omar this is yet another changes</>
+    <h3>Hi Omar this is yet another changes</h3>
   </body>
 </html>;
